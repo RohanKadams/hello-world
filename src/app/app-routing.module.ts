7c1fce981b8ed78f655560423ae66e2bf622f855
RohanKadams <rohanr.kadam12@gmail.com>
2021-02-22 15:53:29 +0530

    Commited server errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,4 +8,4 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { console.log("example")}
+export class AppRoutingModule { }
