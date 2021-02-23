44e277c32d64cff588953f7642474ab0ee819563
RohanKadams <rohanr.kadam12@gmail.com>
2021-02-23 15:33:05 +0530

    Commite all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RouteappComponent } from './routeapp/routeapp.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:"string",component:RouteappComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
