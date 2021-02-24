6394e6d248557b79e8164c1a2350e7b6c776a826
RohanKadams <rohanr.kadam12@gmail.com>
2021-02-24 23:41:11 +0530

    AuthGard

---
+++ src/app/applogin/applogin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'loginMenu',
+  templateUrl: './applogin.component.html',
+  styleUrls: ['./applogin.component.css']
+})
+export class ApploginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
